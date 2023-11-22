c7e2779d9ce972c7e7c1278ef6183525b52160f8
tomatoisblue <moto.by.handball@gmail.com>
2023-11-22 07:50:02 +0000

    initial commit

---
+++ src/main/java/jp/flowzen/flowzenapi/FlowzenApiApplication.java
@@ -0,0 +1,13 @@
+package jp.flowzen.flowzenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowzenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowzenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/flowzen/flowzenapi/FlowzenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.flowzen.flowzenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowzenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
